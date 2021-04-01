e07b82a600ddcdceb4cbd834e49980511cda0b57
Dmitry Volkov <hyardlung@gmail.com>
2021-04-01 20:03:57 +0700

    feat: добавить ProtectedRoute, настроить роуты
    Добавил компонент ProtectedRoute, он защищает от неавторизированного
    перехода на компонент Main, перенаправляет пользователя на страницу
    входа. В браузере не происходит автоматический редирект при изменении
    стейта с true на false, нужно разобраться почему.

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,25 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import '../index.css';
-import {api} from '../utils/api';
+import { api } from '../utils/api';
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
 import PopupWithForm from './PopupWithForm';
 import ImagePopup from './ImagePopup';
-import {CurrentUserContext} from '../contexts/CurrentUserContext';
+import { CurrentUserContext } from '../contexts/CurrentUserContext';
 import EditProfilePopup from './EditProfilePopup';
 import EditAvatarPopup from './EditAvatarPopup';
 import AddPlacePopup from './AddPlacePopup';
 
+import { ProtectedRoute } from './ProtectedRoute';
+
 
 export default function App() {
   // СТЕЙТ-ПЕРЕМЕННЫЕ
+  let [loggedIn] = useState(false);
+
   const [cards, setCards] = useState([]);
   const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = useState(false);
   const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = useState(false);
@@ -54,8 +59,8 @@ export default function App() {
     setIsEditAvatarPopupOpen(true);
   }
   // открытие попапа предпросмотра карточки
-  const handleCardClick = ({name, link}) => {
-    setSelectedCard({name, link});
+  const handleCardClick = ({ name, link }) => {
+    setSelectedCard({ name, link });
     setIsImagePopupOpen(true);
   }
   // лайк карточки
@@ -121,51 +126,60 @@ export default function App() {
 
 
   return (
-    <CurrentUserContext.Provider value={currentUser}>
-      <div className="page root__page" onKeyDown={handleEscClose} >
+    <CurrentUserContext.Provider value={ currentUser }>
+      <div className="page root__page" onKeyDown={ handleEscClose } >
         <Header />
-        <Main
-            onEditProfile={handleEditProfileClick}
-            onAddPlace={handleAddPlaceClick}
-            onEditAvatar={handleEditAvatarClick}
-            cards={cards}
-            onCardClick={handleCardClick}
-            onCardLike={handleCardLike}
-            onCardDelete={handleCardDelete}
-        />
+        <Switch>
+          <ProtectedRoute
+              exact path="/main"
+              loggedIn={ loggedIn }
+              component={ Main }
+              onEditProfile={ handleEditProfileClick }
+              onAddPlace={ handleAddPlaceClick }
+              onEditAvatar={ handleEditAvatarClick }
+              cards={ cards }
+              onCardClick={ handleCardClick }
+              onCardLike={ handleCardLike }
+              onCardDelete={ handleCardDelete }
+          >
+          </ProtectedRoute>
+          <Route path="sign-up" />
+          <Route path="sign-in" />
+        </Switch>
+
         <Footer />
       </div>
 
       {/*попап редактирования профиля*/}
       <EditProfilePopup
-          isOpen={isEditProfilePopupOpen}
-          onClose={closeAllPopups}
-          onCloseOverlay={handleCloseOverlay}
-          onUpdateUser={handleUpdateUser}
+          isOpen={ isEditProfilePopupOpen }
+          onClose={ closeAllPopups }
+          onCloseOverlay={ handleCloseOverlay }
+          onUpdateUser={ handleUpdateUser }
       />
 
       {/*попап редактирования аватара пользователя*/}
       <EditAvatarPopup
-          isOpen={isEditAvatarPopupOpen}
-          onClose={closeAllPopups}
-          onCloseOverlay={handleCloseOverlay}
-          onUpdateAvatar={handleUpdateAvatar}
+          isOpen={ isEditAvatarPopupOpen }
+          onClose={ closeAllPopups }
+          onCloseOverlay={ handleCloseOverlay }
+          onUpdateAvatar={ handleUpdateAvatar }
       />
 
       {/*попап добавления новой карточки*/}
       <AddPlacePopup
-          isOpen={isAddPlacePopupOpen}
-          onClose={closeAllPopups}
-          onCloseOverlay={handleCloseOverlay}
-          onAddPlace={handleAddPlaceSubmit}
+          isOpen={ isAddPlacePopupOpen }
+          onClose={ closeAllPopups }
+          onCloseOverlay={ handleCloseOverlay }
+          onAddPlace={ handleAddPlaceSubmit }
       />
 
       {/*попап предпросмотра изображения карточки*/}
       <ImagePopup
-          isOpen={isImagePopupOpen}
-          onClose={closeAllPopups}
-          onCloseOverlay={handleCloseOverlay}
-          {...selectedCard}
+          isOpen={ isImagePopupOpen }
+          onClose={ closeAllPopups }
+          onCloseOverlay={ handleCloseOverlay }
+          { ...selectedCard }
       />
 
       {/*попап подтверждения удаления карточки*/}
