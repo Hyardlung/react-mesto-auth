361c526a82a58812e022af97e167f294631f827c
Dmitry Volkov <hyardlung@gmail.com>
2021-04-10 21:41:46 +0700

    feat: добавить адаптивность экранам аутентификации
    На маленьких разрешениях инпуты и кнопки экранов входа и регистрации
    больше не вызывают горизонтальный скролл.

--- src/components/InfoTooltip.js
+++ src/components/InfoTooltip.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export const InfoTooltip = ({isOpen, onClose, onCloseOverlay, infoMessage}) => {
   return (
-      <div className={`popup ${isOpen && "popup_opened"}`} onClick={onCloseOverlay}>
-        <div className="popup__container">
+      <div className={`popup popup_tooltip ${isOpen && "popup_opened"}`} onClick={onCloseOverlay}>
+        <div className="popup__container popup__container_tooltip">
           <img className="popup__tooltip-img" src={infoMessage.icon} alt="#"/>
           <h2 className="popup__caption">{infoMessage.caption}</h2>
           <button
