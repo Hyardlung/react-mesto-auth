a5a7c4b6fd03361e3875c81cc5d6f97593699a2d
Dmitry Volkov <hyardlung@gmail.com>
2021-05-13 21:50:07 +0700

    feat: поменять адрес BASE_URL
    Также убрал Bearer из заголовка авторизации метода getContent. И добавил
    скрипт для деплоя в package.json.

--- src/auth.js
+++ src/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://auth.nomoreparties.co';
+export const BASE_URL = 'https://api.hyardlung-frontend.nomoredomains.icu;';
 
 export const register = ({email, password}) => {
   return fetch(`${BASE_URL}/signup`, {
@@ -31,7 +31,7 @@ export const getContent = (token) => {
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json',
-      'Authorization': `Bearer ${token}`
+      'Authorization': `${token}`
     }
   })
       .then(checkResponse)
